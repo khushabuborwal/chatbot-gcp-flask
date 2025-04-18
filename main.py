f80a4a4d28c1f6ca00ee28755c9d197b4e05fc4b
Khushbu Borwal <khushbuborwal@Khushbus-Air.lan>
2025-04-18 15:07:25 -0400

    Added webhook

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json()
+    intent = req.get("queryResult", {}).get("intent", {}).get("displayName")
+    params = req.get("queryResult", {}).get("parameters", {})
 
-if __name__ == "__main__":
-    app.run()
+    firstName = params.get("firstName", "User")
+    lastName = params.get("lastName", "")
+    email = params.get("email", "unknown")
+
+    if intent == "End Chat":
+        reply = f"Thanks again, {firstName} {lastName}! " \
+                f"Your email is: {email}. " \
+                f"This chatbot was created by Khushbu Borwal (borwalkh@mail.uc.edu). " \
+                f"Have a great day!"
+    else:
+        reply = "Hi there! How can I help you?"
+
+    return jsonify({"fulfillmentText": reply})
